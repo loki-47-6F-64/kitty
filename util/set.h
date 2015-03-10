8f6765fa8b501fdffffd92792af8fdc3fa478b2a
Loki <loki-47-6F-64@users.noreply.github.com>
2015-03-10 15:11:38 +0100

    Improved set

--- util/set.h
+++ util/set.h
@@ -11,163 +11,199 @@
 #include <kitty/util/optional.h>
 
 namespace util {
-template<class Container, class It>
-Container concat(It begin, It end) {
-  Container str;
-  
-  for(;begin < end; ++begin) {
-    std::copy(std::begin(*begin), std::end(*begin), std::back_inserter(str));
-  }
-  
-  return str;
-}
-
-template<class Container, class Array>
-Container concat(Array &&container) {
-  return concat<Container>(std::begin(container), std::end(container));
-}
-
-template<class From, class Function>
-inline auto map(From &&from, Function f) -> std::vector<decltype(f(*std::begin(from)))> {
-  typedef decltype(f(*std::begin(from))) input_type;
-  return map_if(std::forward<From>(from), [&](input_type &input) {
-    return util::Optional<input_type> { f(input) };
-  });
-}
-
-template<class From, class Function>
-inline auto map_if(From &&from, Function f) -> 
-  std::vector<
-    typename std::remove_const<
-      typename std::remove_reference<decltype(*f(*std::begin(from)))>::type
-    >::type
-  > 
-{
-  typedef
-    typename std::remove_const<
-      typename std::remove_reference<decltype(*f(*std::begin(from)))>::type
-    >::type input_type;
-  
-  std::vector<input_type> result;
-  result.reserve(std::distance(std::begin(from), std::end(from)));
-  
-  for(auto &input : from) {
-    auto optional = f(input);
+  template<class Container, class It>
+  Container concat(It begin, It end) {
+    Container str;
     
-    if(optional) {
-      result.emplace_back(std::move(*optional));
+    for(;begin < end; ++begin) {
+      std::copy(std::begin(*begin), std::end(*begin), std::back_inserter(str));
     }
+    
+    return str;
   }
   
-  return result;
-}
-
-
-template<class Container, class Function>
-typename std::remove_reference<Container>::type
-copy_if(Container &&from, Function f) {
-  typename std::remove_reference<Container>::type result;
+  template<class Container, class Array>
+  Container concat(Array &&container) {
+    return concat<Container>(std::begin(container), std::end(container));
+  }
   
-  std::copy_if(std::begin(from), std::end(from), std::back_inserter(result), f);
-  
-  return result;
-}
-
-template<class Container, class Function>
-typename std::remove_reference<Container>::type
-move_if(Container &&from, Function f) {
-  typename std::remove_reference<Container>::type result;
-  
-  std::copy_if(
-    std::make_move_iterator(std::begin(from)),
-    std::make_move_iterator(std::end(from)),
-    std::back_inserter(result),
-    f
-  );
-  
-  return result;
-}
-
-
-template<class To, class It>
-To copy_to(It begin, It end) {
-  return To(begin, end);
-}
-
-template<class To, class From>
-To copy_to(From &&from) {
-  return copy_to<To>(std::begin(from), std::end(from));
-}
-
-template<class To, class It>
-To move_to(It begin, It end) {
-  return copy_to<To>(
-    std::make_move_iterator(begin),
-    std::make_move_iterator(end)
-  );
-}
-
-template<class To, class From>
-To move_to(From &&from) {  
-  return move_to<To>(std::begin(from), std::end(from));
-}
-
-template<class Container, class Type>
-std::vector<typename std::remove_reference<Container>::type>
-split(Container &&container, const Type &&val) {
-  
-  std::vector<typename std::remove_reference<Container>::type> vecContainer;
-  
-  auto begin = std::begin(container);
-  for(auto end = begin; end < std::end(container); ++end) {
-    if(val == *end && begin < end) {
-      vecContainer.emplace_back(begin, end);
-      begin = end;
+  template<class It, class Function>
+  inline auto map_if(It begin, It end, Function f) -> 
+  std::vector<
+  typename std::remove_const<
+  typename std::remove_reference<decltype(*f(*begin))>::type
+  >::type
+  >
+  {
+    typedef
+    typename std::remove_const<
+    typename std::remove_reference<decltype(*f(*begin))>::type
+    >::type output_t;
+    
+    std::vector<output_t> result;
+    result.reserve(std::distance(begin, end));
+    
+    for(;begin < end; ++begin) {
+      auto optional = f(*begin);
+      
+      if(optional) {
+        result.emplace_back(std::move(*optional));
+      }
     }
+    
+    return result;
   }
   
-  return vecContainer;
-}
-
-
-template<class Container, class Function>
-bool any(Container &&container, Function f) {
-  for(auto &input : container) {
-    if(f(input))
-      return true;
+  template<class From, class Function>
+  inline auto map_if(From &&from, Function f) -> decltype(map_if(std::begin(from), std::end(from), f))
+  {
+    return map_if(std::begin(from), std::end(from), f);
   }
   
-  return false;
-}
-
-template<class T, class... Args>
-struct First {
-  typedef T type;
-};
-
-
-template<std::size_t N, class Array, class Arg>
-Array _make_array(Array &array, Arg arg) {
-  array[std::tuple_size<Array>::value - N] = std::move(arg);
+  template<class It, class Function>
+  inline auto map(It begin, It end, Function f) ->
+  std::vector<
+  typename std::remove_const<
+  typename std::remove_reference<decltype(f(*begin))>::type
+  >::type
+  >
+  {
+    typedef decltype(*begin) input_t;
+    typedef typename std::remove_const<
+    typename std::remove_reference<decltype(f(*begin))>::type
+    >::type output_t;
+    
+    return map_if(begin, end, [&](input_t &input) {
+      return util::Optional<output_t> { f(input) };
+    });
+  }
   
-  return array;
-}
-
-
-template<std::size_t N, class Array, class Arg, class... Args>
-Array _make_array(Array &array, Arg arg, Args&& ... args) {
-  array[std::tuple_size<Array>::value - N] = std::move(arg);
+  template<class From, class Function>
+  inline auto map(From &&from, Function f) -> decltype(map(std::begin(from), std::end(from), f)) {
+    return map(std::begin(from), std::end(from), f);
+  }
   
-  return _make_array<N - 1>(array, std::forward<Args>(args)...);
-}
-
-template<class... Args, std::size_t N = sizeof... (Args)>
-std::array<typename First<Args...>::type, N> make_array(Args&& ... args) {
-  typedef std::array<typename First<Args...>::type, N> Array;
+  template<class It, class Function>
+  inline auto fold(It begin, It end, Function f) -> decltype(f(*begin, *begin)) {
+    if(begin + 1 == end) {
+      return *begin;
+    }
+    
+    auto result = fold(begin + 1, end, f);
+    
+    return f(*begin, result);
+  }
   
-  Array arr;
-  return _make_array<std::tuple_size<Array>::value - 1>(arr, std::forward<Args>(args)...);  
-}
-
+  template<class From, class Function>
+  inline auto fold(From &&from, Function f) -> decltype(fold(std::begin(from), std::end(from), f)) {
+    return fold(std::begin(from), std::end(from), f);
+  }
+  
+  template<class Container, class Function>
+  typename std::remove_reference<Container>::type
+  copy_if(Container &&from, Function f) {
+    typename std::remove_reference<Container>::type result;
+    
+    std::copy_if(std::begin(from), std::end(from), std::back_inserter(result), f);
+    
+    return result;
+  }
+  
+  template<class Container, class Function>
+  typename std::remove_reference<Container>::type
+  move_if(Container &&from, Function f) {
+    typename std::remove_reference<Container>::type result;
+    
+    std::copy_if(
+      std::make_move_iterator(std::begin(from)),
+                 std::make_move_iterator(std::end(from)),
+                 std::back_inserter(result),
+                 f
+    );
+    
+    return result;
+  }
+  
+  
+  template<class To, class It>
+  To copy_to(It begin, It end) {
+    return To(begin, end);
+  }
+  
+  template<class To, class From>
+  To copy_to(From &&from) {
+    return copy_to<To>(std::begin(from), std::end(from));
+  }
+  
+  template<class To, class It>
+  To move_to(It begin, It end) {
+    return copy_to<To>(
+      std::make_move_iterator(begin),
+                       std::make_move_iterator(end)
+    );
+  }
+  
+  template<class To, class From>
+  To move_to(From &&from) {  
+    return move_to<To>(std::begin(from), std::end(from));
+  }
+  
+  template<class Container, class Type>
+  std::vector<typename std::remove_reference<Container>::type>
+  split(Container &&container, const Type &&val) {
+    
+    std::vector<typename std::remove_reference<Container>::type> vecContainer;
+    
+    auto begin = std::begin(container);
+    for(auto end = begin; end < std::end(container); ++end) {
+      if(val == *end && begin < end) {
+        vecContainer.emplace_back(begin, end);
+        begin = end;
+      }
+    }
+    
+    return vecContainer;
+  }
+  
+  
+  template<class Container, class Function>
+  bool any(Container &&container, Function f) {
+    for(auto &input : container) {
+      if(f(input))
+        return true;
+    }
+    
+    return false;
+  }
+  
+  template<class T, class... Args>
+  struct First {
+    typedef T type;
+  };
+  
+  
+  template<std::size_t N, class Array, class Arg>
+  Array _make_array(Array &array, Arg arg) {
+    array[std::tuple_size<Array>::value - N] = std::move(arg);
+    
+    return array;
+  }
+  
+  
+  template<std::size_t N, class Array, class Arg, class... Args>
+  Array _make_array(Array &array, Arg arg, Args&& ... args) {
+    array[std::tuple_size<Array>::value - N] = std::move(arg);
+    
+    return _make_array<N - 1>(array, std::forward<Args>(args)...);
+  }
+  
+  template<class... Args, std::size_t N = sizeof... (Args)>
+  std::array<typename First<Args...>::type, N> make_array(Args&& ... args) {
+    typedef std::array<typename First<Args...>::type, N> Array;
+    
+    Array arr;
+    return _make_array<std::tuple_size<Array>::value - 1>(arr, std::forward<Args>(args)...);  
+  }
+  
 }
 #endif
