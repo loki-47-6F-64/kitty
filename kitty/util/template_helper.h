a36da67069d3a7b716c68bc975461eec58bc77c3
Loki <marcoblees@noxqslabs.nl>
2015-04-21 11:56:12 +0200

    Check if template parameters contains an instantiation of a specific template

--- kitty/util/template_helper.h
+++ kitty/util/template_helper.h
@@ -4,10 +4,23 @@
 #include <type_traits>
 
 namespace util {
-template<template<typename...> class X, typename T>
+  
+// True if <Optional, Optional<T>>
+template<template<typename...> class X, class T, class...Y>
 struct instantiation_of : public std::false_type {};
 
-template<template<typename...> class X, typename... Y>
-struct instantiation_of<X, X<Y...>> : public std::true_type {};
+template<template<typename...> class X, class T, class... Y>
+struct instantiation_of<X, X<T, Y...>> : public std::true_type {};
+
+// True if <Optional, X<Optional<T>>>
+template<template<typename...> class X, class T, class...Y>
+struct contains_instantiation_of : public std::false_type {};
+  
+template<template<typename...> class X, class T, class... Y>
+struct contains_instantiation_of<X, X<T, Y...>> : public std::true_type {};
+  
+template<template<typename...> class X, template<typename...> class T, class... Y>
+struct contains_instantiation_of<X, T<Y...>> : public contains_instantiation_of<X, Y...> {};
+
 }
 #endif
